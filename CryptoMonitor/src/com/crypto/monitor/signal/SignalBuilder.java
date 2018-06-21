4f364a34a4234c961c577890235bd57f16a7d216
trombio <trombio.michel@gmail.com>
2018-06-21 13:52:55 -0500

    Update SignalBuilder.java

--- CryptoMonitor/src/com/crypto/monitor/signal/SignalBuilder.java
+++ CryptoMonitor/src/com/crypto/monitor/signal/SignalBuilder.java
@@ -1,13 +1,24 @@
 package com.crypto.monitor.signal;
 
+import org.ta4j.core.Indicator;
+import com.crypto.jmes.ta.TAIndicator;
+
 public class SignalBuilder {
-	
+
 	private Signal signal;
-	
-	public SignalBuilder() {
-		
+
+	public SignalBuilder(String simbol) {
+		signal = new Signal();
+		signal.setSymbol(simbol);
 	}
-	
-	
+
+	public void addTrendIndicator(TAIndicator ta, Indicator dataOrigin){
+		signal.addIndicator(ta.analyzeTrend(dataOrigin));
+	}
+
+
+	public Signal build(){
+		return signal;
+	}
 
 }
