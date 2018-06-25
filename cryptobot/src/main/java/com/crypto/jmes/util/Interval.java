e51d65798929ced42d881210a30fc7bf844ec6ae
trombio <trombio.michel@gmail.com>
2018-06-25 18:33:11 -0500

    Update Interval.java

--- cryptobot/src/main/java/com/crypto/jmes/util/Interval.java
+++ cryptobot/src/main/java/com/crypto/jmes/util/Interval.java
@@ -1,11 +1,21 @@
 package com.crypto.jmes.util;
 
 public enum Interval {
-	
-	HOUR,
-	FOUR_HOUR,
-	SIX_HOUR,
-	DAILY,
-	WEEKLY
+
+	HOUR(1),
+	FOUR_HOUR(4),
+	SIX_HOUR(6),
+	DAILY(1),
+	WEEKLY(1);
+
+	private int value;
+
+	Interval(int value){
+		this.value = value;
+	}
+
+	public int getValue(){
+		return value;
+	}
 
 }
